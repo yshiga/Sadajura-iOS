4a38067f5b7ff924b8ca8774166d8894b85bdb58
shiga yuichi <yuichi.shiga@gmail.com>
2015-08-02 10:59:38 -0700

    wishlist

--- NotificationChat/Classes/WishlistView.m
+++ NotificationChat/Classes/WishlistView.m
@@ -84,11 +84,19 @@
 {
     UITableViewCell* cell = [[UITableViewCell alloc]init];
     
-    UILabel* title = [[UILabel alloc] initWithFrame:CGRectMake(20, 0, 190, 44)];
+    UILabel* title = [[UILabel alloc] initWithFrame:CGRectMake(100, 0, 190, 44)];
     [title setText:@"test"];
     [title setFont:[UIFont systemFontOfSize:25]];
     [title setTextColor:[UIColor blackColor]];
     [cell.contentView addSubview:title];
+    
+    
+    UIImage *image = [UIImage imageNamed:@"tab_people"];
+    UIImageView *imageView = [[UIImageView alloc]init];
+    imageView.frame = CGRectMake(20, 0, 44, 44);
+    imageView.image = image;
+    
+    [cell.contentView addSubview:imageView];
     
     UISwitch *check = [[UISwitch alloc] initWithFrame:CGRectMake(260, 5, 40, 40)];
     [check addTarget:self action:@selector(switchToggled:) forControlEvents: UIControlEventTouchUpInside];
