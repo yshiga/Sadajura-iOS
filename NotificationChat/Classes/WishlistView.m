98434cae0608e779e5cbaf2a4d2449dd8b678341
shiga yuichi <yuichi.shiga@gmail.com>
2015-08-02 11:29:20 -0700

    add dammy data

--- NotificationChat/Classes/WishlistView.m
+++ NotificationChat/Classes/WishlistView.m
@@ -75,7 +75,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 {
-	return 3;
+	return 5;
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
@@ -84,14 +84,48 @@
 {
     UITableViewCell* cell = [[UITableViewCell alloc]init];
     
-    UILabel* title = [[UILabel alloc] initWithFrame:CGRectMake(100, 0, 190, 44)];
-    [title setText:@"test"];
-    [title setFont:[UIFont systemFontOfSize:25]];
+    UILabel* title = [[UILabel alloc] initWithFrame:CGRectMake(70, 0, 180, 44)];
+    
+    NSString* name;
+    if(indexPath.row == 0) {
+        name = @"Legacy Khaki - Pleated $25";
+    } else if(indexPath.row == 1) {
+        name = @"Women's Air Max 360 $10";
+    } else if(indexPath.row == 2) {
+        name = @"Hampton Big Pony Shirt $20";
+    } else if(indexPath.row == 3) {
+        name = @"Big Pony Skinny Polo";
+    } else if(indexPath.row == 4) {
+        name = @"Birthday Party Supplies";
+    } else if(indexPath.row == 5) {
+        name = @"Collection Legacy Khaki";
+    }
+    
+    
+    [title setNumberOfLines:2];
+    [title setText:name];
+    [title setFont:[UIFont systemFontOfSize:18]];
     [title setTextColor:[UIColor blackColor]];
     [cell.contentView addSubview:title];
     
+    NSString* imgName;
+    if(indexPath.row == 0) {
+        imgName = @"legackhaki.jpeg";
+    } else if(indexPath.row == 1) {
+        imgName = @"hampton.jpeg";
+    } else if(indexPath.row == 2) {
+        imgName = @"airmax.jpeg";
+    } else if(indexPath.row == 3) {
+        imgName = @"legackhaki.jpeg";
+    } else if(indexPath.row == 4) {
+        imgName = @"birthdays.jpeg";
+    } else if(indexPath.row == 5) {
+        imgName = @"airmax.jpeg";
+    }
     
-    UIImage *image = [UIImage imageNamed:@"tab_people"];
+    
+    
+    UIImage *image = [UIImage imageNamed:imgName];
     UIImageView *imageView = [[UIImageView alloc]init];
     imageView.frame = CGRectMake(20, 0, 44, 44);
     imageView.image = image;
