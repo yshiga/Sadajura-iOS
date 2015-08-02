e7d67ef37dcd95a2d15ad2fd7dddc77a027ab00d
shiga yuichi <yuichi.shiga@gmail.com>
2015-08-02 10:34:26 -0700

    simple wishlist prototype

--- NotificationChat/Classes/WishlistView.m
+++ NotificationChat/Classes/WishlistView.m
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "WishlistView.h"
+#import "UIColor+JSQMessages.h"
 
 
 @interface WishlistView()
@@ -20,6 +21,7 @@
 
 
 
+/*
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 {
@@ -31,13 +33,24 @@
 	}
 	return self;
 }
+ */
 
+-(id)init {
+    self = [super init];
+	if (self)
+	{
+		[self.tabBarItem setImage:[UIImage imageNamed:@"tab_people"]];
+		self.tabBarItem.title = @"wishlist";
+	}
+	return self;
+}
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 - (void)viewDidLoad
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 {
     [super viewDidLoad];
 	self.title = @"Wishlist";
+    
 	//---------------------------------------------------------------------------------------------------------------------------------------------
 //	self.tableView.tableHeaderView = viewHeader;
 }
@@ -48,6 +61,59 @@
 {
 	[super viewDidAppear:animated];
 	//---------------------------------------------------------------------------------------------------------------------------------------------
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+{
+	return 1;
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+{
+	return 3;
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+{
+    UITableViewCell* cell = [[UITableViewCell alloc]init];
+    
+    UILabel* title = [[UILabel alloc] initWithFrame:CGRectMake(20, 0, 190, 44)];
+    [title setText:@"test"];
+    [title setFont:[UIFont systemFontOfSize:25]];
+    [title setTextColor:[UIColor blackColor]];
+    [cell.contentView addSubview:title];
+    
+    UISwitch *check = [[UISwitch alloc] initWithFrame:CGRectMake(260, 5, 40, 40)];
+    [check addTarget:self action:@selector(switchToggled:) forControlEvents: UIControlEventTouchUpInside];
+    
+    check.tag = indexPath.row;
+    
+    [cell.contentView addSubview:check];
+    
+	return cell;
+}
+
+- (void) switchToggled:(id)sender {
+    UISwitch *mySwitch = (UISwitch *)sender;
+    
+    
+    
+}
+
+#pragma mark - Table view delegate
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+{
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
